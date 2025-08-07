8e1b5d62a6f748399b9a1d3cc1c32f32fb1c1f00
Jiajun Liang <Jiajunlwork@gmail.com>
2025-08-07 00:14:03 -0400

    Update sumAll.js/.spec.js
    Complete exercise

--- 09_sumAll/sumAll.js
+++ 09_sumAll/sumAll.js
@@ -1,5 +1,26 @@
-const sumAll = function() {
+const sumAll = function(arg1, arg2) {
+    if(typeof arg1 == "number" && typeof arg2 == "number"){
+        if(arg1 < 0 || arg2 < 0 || Math.floor(arg1) != arg1 || Math.floor(arg2) != arg2)
+        {
+            return "ERROR";
+        }
+        let startInt = arg1, endInt = arg2, sumTotal = 0;
+        
+        if(arg1 > arg2){
+            startInt = arg2;
+            endInt = arg1;
+        } 
+    
+        for(let i = startInt; i <= endInt; i++){
+            sumTotal += i;
+        }
 
+        return sumTotal;
+
+    }else
+    {
+        return "ERROR";
+    }
 };
 
 // Do not edit below this line

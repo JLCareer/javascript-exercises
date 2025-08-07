28dad4a7d59a036d21e919614df3e01ef555e126
Jiajun Liang <Jiajunlwork@gmail.com>
2025-08-06 23:57:07 -0400

    Update removeFromArray.js/.spec.js
    Complete exercise

--- 08_removeFromArray/removeFromArray.js
+++ 08_removeFromArray/removeFromArray.js
@@ -1,4 +1,5 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr, ...removeItems) {    
+    return arr.filter(item => !removeItems.includes(item))
 };
 
 // Do not edit below this line

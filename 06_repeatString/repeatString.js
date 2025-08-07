d52332852de07c942cdebe2fb52a8fef0ab3e216
Jiajun Liang <Jiajunlwork@gmail.com>
2025-08-06 20:32:37 -0400

    Update repeatString.js/.spec.js
    Completing the exercise

--- 06_repeatString/repeatString.js
+++ 06_repeatString/repeatString.js
@@ -1,5 +1,14 @@
-const repeatString = function() {
-
+const repeatString = function(string, num) {
+    let finalString = "";
+    if(num >= 0){
+        for(let i = 0; i < num; i++){
+        finalString += string;
+        }
+    }
+    else{
+        finalString += "ERROR"
+    }
+    return finalString;
 };
 
 // Do not edit below this line
